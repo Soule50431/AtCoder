54ffa62cabdf2d7599f2010e0fe0852c9022cbfa
soule50431 <h.ekcero.el6no11@outlook.jp>
2021-05-07 17:36:57 +0900

    ABC 195 途中

---
+++ ABC/ABC 195/d.py
@@ -0,0 +1,11 @@
+n, m, q = map(int, input().split())
+
+baggage = []
+
+for i in range(n):
+    baggage.append(list(map(int, input().split())))
+
+box = list(map(int, input().split()))
+print(baggage)
+print(box)
+
